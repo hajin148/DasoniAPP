fdfc51ed6f913b718ad219c065d28eb0c75514a7
Dana <fouryou504@gmail.com>
2023-12-06 16:26:56 -0700

    song name implementation

--- app/src/main/java/com/example/dasoniapp/RhythmGameActivity.kt
+++ app/src/main/java/com/example/dasoniapp/RhythmGameActivity.kt
@@ -11,7 +11,6 @@ import android.view.View
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.view.animation.LinearInterpolator
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 
