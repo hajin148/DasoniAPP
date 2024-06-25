3626d40323b4743cba1e2d8a2d4ae04d73d542fa
Hajin_Jeon <u1437827@umail.utah.edu>
2024-06-24 21:52:28 -0600

    Tablet Ver 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
     buildTypes {
         release {
 
-            //isDebuggable = false
+            isDebuggable = false
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
 
