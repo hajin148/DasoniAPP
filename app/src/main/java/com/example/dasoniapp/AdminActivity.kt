e00d250a7c7acf7e1f22c9fe6ec932c55bbbe5d7
Hajin_Jeon <u1437827@umail.utah.edu>
2024-07-18 17:21:41 -0600

    Button Background Selector

--- app/src/main/java/com/example/dasoniapp/AdminActivity.kt
+++ app/src/main/java/com/example/dasoniapp/AdminActivity.kt
@@ -1,12 +1,20 @@
 package com.example.dasoniapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 
 class AdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.admin_main)
+
+        val btnUserManagement: Button = findViewById(R.id.btn_admin_user_management)
+        val indicatorUserManagement: View = findViewById(R.id.indicator_user_management)
+
+        // Show the indicator when the button is selected
+        indicatorUserManagement.visibility = View.VISIBLE
 
         // Add the fragment if it's not already added
         if (savedInstanceState == null) {
